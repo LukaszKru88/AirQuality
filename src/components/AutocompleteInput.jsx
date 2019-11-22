e87fb344dfd66690d7deb4683dadf1ae8ffb503a
LukaszKru88 <luk.kruszelnicki.programista@gmail.com>
2019-11-22 19:33:57 +0100

    Added autocomplite input component

--- src/components/AutocompleteInput.jsx
+++ src/components/AutocompleteInput.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 class AutocompleteInput extends Component {
     state = {
+        activeSuggestion: 0,
         userInput: "",
         filteredSuggestions: [],
         showSuggestions: false
@@ -23,45 +24,91 @@ class AutocompleteInput extends Component {
             suggestion =>
                 suggestion.name.toLowerCase().indexOf(input.value.toLowerCase()) > -1
         );
-        console.log(filteredSuggestions)
 
         this.setState({
+            activeSuggestion: 0,
             userInput: input.value,
             filteredSuggestions,
             showSuggestions: true
         })
     }
 
+    onClick = ({ currentTarget: input }) => {
+        this.setState({
+            activeSuggestion: 0,
+            filteredSuggestions: [],
+            showSuggestions: false,
+            userInput: input.innerText
+        });
+    }
+
+    // onKeyDown = e => {
+    //     const { activeSuggestion, filteredSuggestions } = this.state;
+
+    //     if (e.keyCode === 13) {
+    //         this.setState({
+    //             activeSuggestion: 0,
+    //             showSuggestions: false,
+    //             userInput: filteredSuggestions[activeSuggestion]
+    //         });
+    //     }
+
+    //     else if (e.keyCode === 38) {
+    //         if (activeSuggestion === 0) {
+    //             return;
+    //         }
+
+    //         this.setState({ activeSuggestion: activeSuggestion - 1 });
+    //     }
+
+    //     else if (e.keyCode === 40) {
+    //         if (activeSuggestion - 1 === filteredSuggestions.length) {
+    //             return;
+    //         }
+
+    //         this.setState({ activeSuggestion: activeSuggestion + 1 });
+    //     }
+    // };
+
     render() {
         const { userInput, filteredSuggestions, showSuggestions } = this.state;
+        let suggestionsList;
 
-        function suggestionsList() {
-            if (userInput && showSuggestions) {
-                if (filteredSuggestions.length) {
-                    return (
-                        <div className="row">
-                            <ul className="suggestions offset-md-3 col-md-6">
-                                {filteredSuggestions.map((suggestion, index) => (
-                                    <li key={index}>{suggestion.name}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    )
-                }
+        if (userInput && showSuggestions) {
+            if (filteredSuggestions.length) {
+                suggestionsList = (
+                    <div className="row">
+                        <ul className="suggestions offset-lg-3 col-lg-6">
+                            {filteredSuggestions.map((suggestion, index) => {
+                                return (
+                                    <li key={index} onClick={this.onClick}>
+                                        {suggestion.name}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                )
+            } else {
+                suggestionsList = (
+                    <div className="text-center offset-lg-3 col-lg-6">
+                        <em>No suggestions</em>
+                    </div>
+                );
             }
         }
 
         return (
             <React.Fragment>
                 <div className="row">
-                    <form className="countryForm offset-md-3 col-md-6" onSubmit={this.onSubmit}>
+                    <form className="countryForm offset-lg-3 col-lg-6" onSubmit={(event) => this.props.onSubmit(event, userInput)}>
                         <div className="input-group">
-                            <input name="country" className="form-control" id="country" type="text" placeholder="Country..." onChange={this.onChange} value={userInput} />
+                            <input name="country" className="form-control" id="country" type="text" placeholder="Country..." onChange={this.onChange} onKeyDown={this.onKeyDown} value={userInput} />
                             <div className="input-group-append"><input className="btn btn-info text-uppercase" type="submit" value="Take a breath" /></div>
                         </div>
                     </form>
                 </div>
-                {suggestionsList()}
+                {suggestionsList}
             </React.Fragment>
         );
     }
