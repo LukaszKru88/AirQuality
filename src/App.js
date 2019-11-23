fd6c27fd43c71454c75a839e0a84e6dcbfaa73e7
LukaszKru88 <luk.kruszelnicki.programista@gmail.com>
2019-11-23 15:08:16 +0100

    Added Accordion component to main app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import AutocompleteInput from './components/AutocompleteInput'
+import { withCookies } from "react-cookie";
+import AutocompleteInput from './components/AutocompleteInput';
+import CitiesAccordion from './components/CitiesAccordion';
 import './App.css';
 
 class App extends React.Component {
@@ -14,20 +16,22 @@ class App extends React.Component {
 
   componentDidMount() {
     this.setState({
-      parameter: "so2",
+      parameter: "pm10",
       limit_results: 100,
       date: this.getCurrentDate(),
+      cities: this.getCitiesFromCookies(),
+      country: this.getCountryFromCookies(),
       suggestions: [
         { name: "Poland", code: "PL" },
         { name: "Germany", code: "DE" },
         { name: "France", code: "FR" },
-        { name: "Spain", code: "ES" }]
+        { name: "Spain", code: "ES" }],
     })
   }
 
   getCurrentDate = () => {
     const date = new Date();
-    const hh = date.getUTCHours() - 1; //-1h because measurement delay on page
+    const hh = date.getUTCHours() - 1; //-1h becuse measurement delay on page
     const dd = date.getDate();
     const mm = date.getMonth() + 1; //Returns 0 for January
     const yyyy = date.getFullYear();
@@ -35,15 +39,28 @@ class App extends React.Component {
     return `${yyyy}-${mm < 10 ? "0" + mm : mm}-${dd < 10 ? "0" + dd : dd}T${hh < 10 ? "0" + hh : hh}:00:00`
   }
 
-  onChange = ({ currentTarget: input }) => {
-    const { name, value } = input;
-    this.setState({ [name]: value });
+  getCitiesFromCookies = () => {
+    const { cookies } = this.props
+    if (cookies.get("pollutedCities") && cookies.get("pollutedCities").length !== 0) {
+      return cookies.get("pollutedCities")
+    }
+  }
+
+  getCountryFromCookies = () => {
+    const { cookies } = this.props
+    if (cookies.get("country")) {
+      return cookies.get("country")
+    }
   }
 
   setCountry = (suggestions, userInput) => {
+    const { cookies } = this.props
+
     for (let suggestion of suggestions) {
-      if (suggestion.name === userInput)
+      if (suggestion.name === userInput) {
+        cookies.set("country", suggestion.name, { path: "/" })
         return suggestion.code;
+      }
     }
     return "XYZ";
   }
@@ -60,14 +77,15 @@ class App extends React.Component {
   onSubmit = async (event, userInput) => {
     event.preventDefault();
     const { parameter, limit_results, date, suggestions } = this.state
+    const { cookies } = this.props
 
     await this.setState({ country: this.setCountry(suggestions, userInput) })
 
-    const url = `https://api.openaq.org/v1/measurements?country=${this.state.country}&parameter=${parameter}&order_by=value&sort=desc&limit=${limit_results}&date_from=${date}&date_to=${date}&format=json`
+    const url = `https://api.openaq.org/v1/measurements?country=${this.state.country}&parameter=${parameter}&order_by=value&sort=desc&limit=${limit_results}&format=json`
 
     let response = await fetch(url);
     response = await response.json();
-    console.log(url)
+
     if (!response.error) {
       let { results } = response;
       results = results.map(result => {
@@ -81,6 +99,7 @@ class App extends React.Component {
       });
 
       results = this.getUniqueCities(results, "city")
+      cookies.set("pollutedCities", results, { path: "/" })
       this.setState({ cities: results });
     }
     else {
@@ -95,12 +114,15 @@ class App extends React.Component {
         <header>
           <h1 className="text-center">Check air quality in Your country</h1>
         </header>
-        <AutocompleteInput
-          suggestions={suggestions}
-          onSubmit={this.onSubmit}
-        />
         <section>
-          <div className="row">
+          <AutocompleteInput
+            suggestions={suggestions}
+            onSubmit={this.onSubmit}
+          />
+        </section>
+        <section>
+          <CitiesAccordion cities={cities} />
+          {/* <div className="row">
             {cities && <table className="citiesTable offset-md-1 col-md-10">
               {cities.length === 0 ?
                 <div className="text-center text-uppercase"><p>No data to view here</p></div> :
@@ -123,7 +145,7 @@ class App extends React.Component {
                 ))}
               </tbody>
             </table>}
-          </div>
+          </div> */}
         </section>
         <footer></footer>
       </div>
@@ -131,4 +153,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withCookies(App);
