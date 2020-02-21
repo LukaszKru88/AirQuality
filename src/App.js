8c6f4f6c60a8567467a7c772adb1f425c862a1f1
LukaszKru88 <luk.kruszelnicki.programista@gmail.com>
2020-02-21 11:58:22 +0100

    localStorage data retrieval

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { withCookies, useCookies } from "react-cookie";
 import axios from 'axios';
 import AutocompleteInput from './components/AutocompleteInput';
 import CitiesAccordion from './components/CitiesAccordion';
@@ -16,23 +15,41 @@ const countries = [
 const parameter = "pm10";
 const resultsLimit = 100;
 
+const getLocalData = (key) => {
+  let localData = localStorage.getItem(key);
+  return localData ? JSON.parse(localData) : [];
+}
+
 function App() {
-  console.log("Render App")
-  const [{ country }, setCountry] = useCookies(["country"]);
-  const [cities, setCities] = useState([]);
+  const [country, setCountry] = useState(getLocalData('country'));
+  const [cities, setCities] = useState(getLocalData('cities'));
   const [isLoading, setIsLoading] = useState(false);
   const firstFetch = useRef(true);
 
+  const handleError = (error) => {
+    toast.error(error.message,
+      {
+        autoClose: 5000,
+        hideProgressBar: true,
+      });
+  }
+
   const getData = callbackCountry => {
-    if (callbackCountry) {
-      console.log("Firing getData() with: ", callbackCountry);
-      setCountry("country", countries.filter(country => country.name.toLowerCase() === callbackCountry.toLowerCase()));
-    } else {
-      toast.error("To check air quality You need to type in country name",
-        {
-          autoClose: 5000,
-          hideProgressBar: true,
-        });
+    try {
+      if (callbackCountry) {
+        const filteredCountry = countries.filter(country => country.name.toLowerCase() === callbackCountry.toLowerCase());
+
+        if (filteredCountry.length) {
+          localStorage.setItem('country', JSON.stringify(filteredCountry));
+          setCountry(filteredCountry);
+        }
+        else
+          throw new Error("No such country in browser");
+      } else {
+        throw new Error("To check air quality You need to type in country name");
+      }
+    } catch (error) {
+      handleError(error);
     }
   }
 
@@ -43,27 +60,20 @@ function App() {
     }
 
     const fetchData = async () => {
-      console.log(country[0].code);
       setIsLoading(true);
-      const response = await axios.get(`https://api.openaq.org/v1/measurements?country=${country[0].code}&parameter=${parameter}&order_by=value&sort=desc&limit=${resultsLimit}&format=json`
-      );
+      const response = await axios.get(`https://api.openaq.org/v1/measurements?country=${country[0].code}&parameter=${parameter}&order_by=value&sort=desc&limit=${resultsLimit}&format=json`);
+      setIsLoading(false);
 
       if (!response.error) {
         let { results } = response.data;
         results = getUniqueCities(results, "city");
+        localStorage.setItem('cities', JSON.stringify(results));
         setCities(results);
-        setIsLoading(false);
       }
-      else {
-        setIsLoading(false);
-        toast.error(response.message, {
-          autoClose: 5000,
-          hideProgressBar: true,
-        });
-      }
+      else
+        handleError(response);
     }
 
-    console.log("Firing fetch");
     fetchData();
   }, [country]);
 
@@ -89,112 +99,17 @@ function App() {
         />
       </section>
       <ToastContainer />
-      {isLoading ? "Loading..." :
+      {isLoading ? <h5>Loading...</h5> :
         <section>
           <CitiesAccordion cities={cities} />
         </section>}
-      {/* footer></footer> */}
+      <footer>
+        <div className="footer offset-md-2 col-md-8">
+          <h5>Always care about Your lungs! Every breath is a essence of life</h5>
+        </div>
+      </footer>
     </div>
   );
 }
 
-export default withCookies(App);
-
-// class App extends React.Component {
-//   state = {
-//     country: "",
-//     parameter: "",
-//     limit_results: null,
-//     cities: [],
-//     suggestions: []
-//   }
-
-//   componentDidMount() {
-//     this.setState({
-//       parameter: getPollutionParameter(),
-//       limit_results: getSearchLimit(),
-//       cities: getCitiesFromCookies(this.props),
-//       country: getCountryFromCookies(this.props),
-//       suggestions: getSuggestions()
-//     })
-//   }
-
-//   setCountry = (suggestions, userInput) => {
-//     const { cookies } = this.props;
-
-//     for (let suggestion of suggestions) {
-//       if (suggestion.name === userInput) {
-//         cookies.set("country", suggestion.name, { path: "/" })
-//         return suggestion.code;
-//       }
-//     }
-//     return "XYZ";
-//   }
-
-//   getUniqueCities = (cities, parameter) => {
-//     return cities
-//       .map(city => city[parameter])
-//       .map((city, i, final) => final.indexOf(city) === i && i)
-//       .filter(city => cities[city])
-//       .map(city => cities[city])
-//       .slice(0, 10);
-//   }
-
-//   onSubmit = async (event, userInput) => {
-//     event.preventDefault();
-//     const { parameter, limit_results, suggestions } = this.state;
-//     const { cookies } = this.props;
-
-//     await this.setState({ country: this.setCountry(suggestions, userInput), cities: [] });
-
-//     const url = `https://api.openaq.org/v1/measurements?country=${this.state.country}&parameter=${parameter}&order_by=value&sort=desc&limit=${limit_results}&format=json`;
-//     let response = await fetch(url);
-//     response = await response.json();
-
-//     if (!response.error) {
-//       let { results } = response;
-//       results = results.map(result => {
-//         return {
-//           city: result.city,
-//           location: result.location,
-//           parameter: result.parameter,
-//           value: Math.round(result.value * 100) / 100,
-//           unit: result.unit
-//         };
-//       });
-
-//       results = this.getUniqueCities(results, "city");
-//       cookies.set("pollutedCities", results, { path: "/" });
-//       this.setState({ cities: results });
-//     }
-//     else {
-//       toast.error(response.message, {
-//         autoClose: 5000,
-//         hideProgressBar: true,
-//       });
-//     }
-//   }
-
-//   render() {
-//     const { cities, suggestions } = this.state
-//     return (
-//       <div className="container">
-//         <header>
-//           <h1 className="text-center">Check air quality in Your country</h1>
-//         </header>
-//         <section>
-//           <AutocompleteInput
-//             suggestions={suggestions}
-//             onSubmit={this.onSubmit}
-//           />
-//           <ToastContainer />
-//         </section>
-//         <section>
-//           <CitiesAccordion cities={cities} />
-//         </section>
-//         <footer></footer>
-//       </div>
-//     );
-//   }
-// }
-
+export default App;
