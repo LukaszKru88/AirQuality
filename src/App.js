6ab69770708602a5427acbff4304df44f7fb796e
LukaszKru88 <luk.kruszelnicki.programista@gmail.com>
2020-03-10 20:26:55 +0100

    Deleted commented out footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
-import axios from 'axios';
-import AutocompleteInput from './components/AutocompleteInput';
-import CitiesAccordion from './components/CitiesAccordion';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import AutocompleteInput from "./components/AutocompleteInput";
+import CitiesAccordion from "./components/CitiesAccordion";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const countries = [
   { id: 1, code: "PL", name: "Poland" },
@@ -15,43 +15,45 @@ const countries = [
 const parameter = "pm10";
 const resultsLimit = 100;
 
-const getLocalData = (key) => {
+const getLocalData = key => {
   let localData = localStorage.getItem(key);
   return localData ? JSON.parse(localData) : [];
-}
+};
 
 function App() {
-  const [country, setCountry] = useState(getLocalData('country'));
-  const [cities, setCities] = useState(getLocalData('cities'));
+  const [country, setCountry] = useState(getLocalData("country"));
+  const [cities, setCities] = useState(getLocalData("cities"));
   const [isLoading, setIsLoading] = useState(false);
   const firstFetch = useRef(true);
 
-  const handleError = (error) => {
-    toast.error(error.message,
-      {
-        autoClose: 5000,
-        hideProgressBar: true,
-      });
-  }
+  const handleError = error => {
+    toast.error(error.message, {
+      autoClose: 5000,
+      hideProgressBar: true
+    });
+  };
 
   const getData = callbackCountry => {
     try {
       if (callbackCountry) {
-        const filteredCountry = countries.filter(country => country.name.toLowerCase() === callbackCountry.toLowerCase());
+        const filteredCountry = countries.filter(
+          country =>
+            country.name.toLowerCase() === callbackCountry.toLowerCase()
+        );
 
         if (filteredCountry.length) {
-          localStorage.setItem('country', JSON.stringify(filteredCountry));
+          localStorage.setItem("country", JSON.stringify(filteredCountry));
           setCountry(filteredCountry);
-        }
-        else
-          throw new Error("No such country in browser");
+        } else throw new Error("No such country in browser");
       } else {
-        throw new Error("To check air quality You need to type in country name");
+        throw new Error(
+          "To check air quality You need to type in country name"
+        );
       }
     } catch (error) {
       handleError(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (firstFetch.current) {
@@ -61,18 +63,20 @@ function App() {
 
     const fetchData = async () => {
       setIsLoading(true);
-      const response = await axios.get(`https://api.openaq.org/v1/measurements?country=${country[0].code}&parameter=${parameter}&order_by=value&sort=desc&limit=${resultsLimit}&format=json`);
+      const response = await axios.get(
+        `https://api.openaq.org/v1/measurements?country=${
+        country[0].code
+        }&parameter=${parameter}&order_by=value&sort=desc&limit=${resultsLimit}&format=json`
+      );
       setIsLoading(false);
 
       if (!response.error) {
         let { results } = response.data;
         results = getUniqueCities(results, "city");
-        localStorage.setItem('cities', JSON.stringify(results));
+        localStorage.setItem("cities", JSON.stringify(results));
         setCities(results);
-      }
-      else
-        handleError(response);
-    }
+      } else handleError(response);
+    };
 
     fetchData();
   }, [country]);
@@ -84,30 +88,28 @@ function App() {
       .filter(city => cities[city])
       .map(city => cities[city])
       .slice(0, 10);
-  }
+  };
 
   return (
     <div className="container">
       <header>
-        <h1 className="text-center m-5">Check air quality in Your country</h1>
+        <h1>Check air quality in Your country</h1>
       </header>
       <section>
         <AutocompleteInput
-          country={country ? country[0].name : ""}
+          country={country.length ? country[0].name : ""}
           countries={countries}
           handleSubmit={getData}
         />
       </section>
       <ToastContainer />
-      {isLoading ? <h5>Loading...</h5> :
-        <section>
-          <CitiesAccordion cities={cities} />
-        </section>}
-      {/* <footer>
-        <div className="footer offset-md-2 col-md-8">
-          <h5>Always care about Your lungs! Every breath is a essence of life</h5>
-        </div>
-      </footer> */}
+      {isLoading ? (
+        <h5>Loading...</h5>
+      ) : (
+          <section>
+            <CitiesAccordion cities={cities} />
+          </section>
+        )}
     </div>
   );
 }
